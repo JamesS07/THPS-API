08d6def8eebaf0cbe7cb202046f2ab8bf448fb2f
James Smead <123837559+JamesS07@users.noreply.github.com>
2023-08-05 17:16:43 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ console.log('Working')
 
 // below is the burger of the day code to search date in the API.  Needs to be tweaked
 
-document.querySelector('button').addEventListener('click', getBurger)
+document.querySelector('button').addEventListener('click', getSkater)
 
-function getBurger(){
-    let epID = document.querySelector('input').value
+function getSkater(){
+    let skaters = document.querySelector('input').value
 
 
-fetch(`https://bobsburgers-api.herokuapp.com/burgerOfTheDay/[${epID}]`)
+fetch(`https://thps-bdf7deda80e6.herokuapp.com/api/[${skaters}]`)
 .then(res => res.json())
 .then(data => {
     console.log(data)
-    document.querySelector('h4').innerText = data[0].name
-    document.querySelector('.season').innerText = data[0].season
-    document.querySelector('.episode').innerText = data[0].episode
+    document.querySelector('.name').innerText = data.name
+    document.querySelector('.age').innerText = data.age
+    document.querySelector('.born').innerText = data.birthLocation
     document.querySelector('.price').innerText = data[0].price
 
 })
--- server.js
+++ server.js
@@ -7,66 +7,77 @@ app.use(cors())
 
 const skaters = {
     'tony hawk':{
+        'name': 'Tony Hawk',
         'age': 55,
         'specialTrick':'The 900',
         'birthLocation': 'San Diego, California',
         'stance': 'Goofy'
     },
     'bob burnquist': {
+        'name': 'Bob Burnquist',
         'age': 46,
         'specialTrick': 'Burntwist',
         'birthLocation': 'Rio de Janeiro, Brazil',
         'stance': 'Regular' 
     },
     'kareem campbell':{
+        'name': 'Kareem Campbell',
         'age': 49,
         'specialTrick': '50 Board Variable',
         'birthLocation': 'Harlem, New York',
         'stance': 'Regular' 
     },
     'rune glifberg':{
+        'name': 'Rune Glifberg',
         'age': 48,
         'specialTrick': 'Kickflip McTwist',
         'birthLocation': 'Copenhagen, Denmark',
         'stance': 'Regular'
     },
     'andrew reynolds':{
+        'name': 'Andrew Reynolds',
         'age': 45,
         'specialTrick':'Heelflip to Buntside',
         'birthLocation': 'Lakeland, Florida',
         'stance': 'Regular' 
     },
     'bucky lasek':{
+        'name': 'Bucky Lasek',
         'age': 50,
         'specialTrick': 'Fingerflip Airwalk',
         'birthLocation': 'Baltimore, Maryland',
         'stance': 'Regular'
     },
     'elissa steamer':{
+        'name': 'Elissa Steamer',
         'age': 47,
         'specialTrick': 'Judo Madonna',
         'birthLocation': 'Fort Myers, Florida',
         'stance': 'Regular'
     },
     'jamie thomas':{
+        'name': 'Jamie Thomas',
         'age': 48,
         'specialTrick':' 540 Flip',
         'birthLocation': 'Palm Beach Gardens, Florida',
         'stance': 'Regular'
     },
     'chad muska':{
+        'name': 'Chad Muska',
         'age': 46,
         'specialTrick':'360 Shove it Rewind',
         'birthLocation': 'Lorain, Ohio',
         'stance': 'Regular'
     },
     'geoff rowley':{
+        'name': 'Geoff Rowley',
         'age': 47,
         'specialTrick': 'Darkslide',
         'birthLocation': 'Liverpool, England',
         'stance': 'Regular'
     },
     'create a skater':{
+        'name': 'Create a Skater',
         'age': 25,
         'specialTrick': 'McTwist',
         'birthLocation': 'USA',
